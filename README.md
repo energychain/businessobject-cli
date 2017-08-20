3278dbdeff0d59d968fc3e2aa847832f77ddaded
Thorsten Zoerner <me@thorsten-zoerner.com>
2017-08-20 02:02:46 +0200

    Updated Event handling to allow Cross Meter Point Reading and other Energy related jumps Updated Readme to have sample file

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # businessobject-cli
 Command Line Utility to work with STROMDAO Energy Blockchain
+
+This is a simple wrapper to the Business Object
+
+## Install
+```
+npm install -g businessobject-cli
+```
+
+## Usage Sample
+```
+$ stromdao
+local@localhost~$ goto /1337/mpr/0x0/storeReading
+/1337/mpr/0x0/storeReading call 123
+goto /1337/mpr/0x0/reading 0x1234...
+```
--- index.js
+++ index.js
@@ -13,76 +13,139 @@ args = f => f.toString ().replace (/[\r\n\s]+/g, ' ').
 var types = [];
 var objects = [];
 var cmds=[];
+var instances = [];
 
 var cliNode = new StromDAONode.Node({external_id:"cli",testMode:true,rpc:rpcurl,abilocation:"https://cdn.rawgit.com/energychain/StromDAO-BusinessObject/master/smart_contracts/"});
 
+function introspectCalls(obj,a,parent) {
+		var a=vorpal._delimiter.split("/");
+		if(vorpal.find("call")) vorpal.find("call").remove("call");
+		extid=a[1];		
+		
+			parent
+			.command("call [arguments...]")
+			.types({string: ['_']})
+			.action(function(args,cb) {					
+				var a=vorpal._delimiter.split("/");
+				extid=a[1];			
+				cliNode = new StromDAONode.Node({external_id:extid,testMode:true,rpc:rpcurl,abilocation:"https://cdn.rawgit.com/energychain/StromDAO-BusinessObject/master/smart_contracts/"});														
+				cargs=[];
+				address=a[3];
+				if(address!="0x0") cargs.push(address);
 
-function introspect(name,obj) {
-	names=Object.getOwnPropertyNames(obj);
-	for(var i=0;i<names.length;i++) {	
-		if((names[i]!="obj")&&(names[i]!="test")) {
-			
-			var x= Object.getOwnPropertyDescriptor(obj,names[i]);	
-				a=args(x.value);
-				var argumentlist=" <extid>";
-				for(var j=0;j<a.length;j++) {
-						if(a[j].length>0) {												
-							if(argumentlist.length>0) argumentlist+=" ";
-							argumentlist+="<"+a[j]+">";
-							if(typeof types[a[j]] != "Array") types[a[j]]=[];
-							//types[a[j]].push(name+"."+names[i]);
-							
-							
-							
+				cliNode[a[2]].apply(this,cargs).then(function(x) {
+					margs=args.arguments;
+					x[a[4]].apply(this,margs).then(function(tx) {
+
+						if(typeof tx=="object") {
+							for (var k in tx){
+								if (tx.hasOwnProperty(k)) {
+									parent.log(k+":",tx[k].toString());								 
+								}
+							}						
+						} else {
+							parent.log(tx);
 						}
-				}	
-				if(typeof cmds[name+" "+names[i]+""+argumentlist]=="undefined") {								
-								cmds[name+" "+names[i]+""+argumentlist]=
-								vorpal
-									  .command(name+" "+names[i]+""+argumentlist)				  
-									  .action(function (args, callback) {
-										this.log(args);									
-										callback();
-								});	
-				}
-		}
-	}			
-	objects.push(name);
+					});							
+				});						
+				cb();
+			});		
+}
+
+function introspectMethods(obj,a,parent) {	
+	cargs=[];
+	address=a[3];
+	if(address!="0x0") cargs.push(address);
+	
+	obj[a[2]].apply(this,cargs).then(function(x) {
+			names=Object.getOwnPropertyNames(x);
+			for(var i=0;i<names.length;i++) {
+					parent.log("Loading ",parent._delimiter+names[i]+"/");	
+					
+			}	
+			parent
+					.command("use <method>")
+					.action(function(args,cb) {	
+						vorpal.delimiter(vorpal._delimiter+""+args.method+"/");
+						var a=vorpal._delimiter.split("/");								
+						introspectCalls(obj,a,parent);			
+						cb();
+				});
+	});		
 	
 }
 
-function introobject(i,node) {
+function introobject(i,node,parent) {
 	var names=Object.getOwnPropertyNames(node);
 	i++;	
-	
 	if(i>=names.length)  {
-		console.log("Initialized");
+		return;
+		//parent.show();
 	} else
 	if(names[i].indexOf("_")!=0) {
-		try{
-
-		
-		var n = names[i];
-		node[names[i]].apply(this,['0x0000000000000000000000000000000000000008']).then(function(x) {
-						introspect(n,x);
-						introobject(i,node);
-		});
-		//introspect(names[i]);	
-		} catch(e) {
-			introobject(i,node);
-		};
-	} else {introobject(i,node);}
+		parent.log("Loading ",parent._delimiter+"/"+names[i]+"/");			
+        introobject(i,node,parent);
+	
+	} else {
+		introobject(i,node,parent);
+	}
 }
 
-introobject(-1,cliNode);
-		
+
+
+vorpal
+  .command('use [extid]')  
+  .action(function (args, callback) {
+    cliNode = new StromDAONode.Node({external_id:args.extid,testMode:true,rpc:rpcurl,abilocation:"https://cdn.rawgit.com/energychain/StromDAO-BusinessObject/master/smart_contracts/"});
+	
+    vorpal
+	.delimiter("/"+args.extid)
+	.show();
+	instances=[];
+    introobject(-1,cliNode,vorpal);
+
+    if(vorpal.find("use")) vorpal.find("use").remove("use");
+    
+	vorpal
+	.command("use <instance>")
+	.option("-a <address>")
+	.types({
+		string: ['a']
+	})
+	.action(function(args,cb) {	
+			if(!args.options.a) args.options.a="0x0";			
+			
+			vorpal
+			.delimiter(vorpal._delimiter+"/"+args.instance+"/"+args.options.a+"/");
+			if(vorpal.find("use")) vorpal.find("use").remove();
+    
+			var a=vorpal._delimiter.split("/");
+			extid=a[1];			
+			cliNode = new StromDAONode.Node({external_id:args.extid,testMode:true,rpc:rpcurl,abilocation:"https://cdn.rawgit.com/energychain/StromDAO-BusinessObject/master/smart_contracts/"});								
+			introspectMethods(cliNode,a,vorpal);
+			cb();			
+							
+	})
+    callback();
+});		
 		
 vorpal
-  .command('account show [extid]')
+   .command("goto <path>")
+   .action(function(args,callback) {
+		vorpal.delimiter(args.path);
+
+		var a=vorpal._delimiter.split("/");								
+					
+		introspectCalls(cliNode,a,vorpal);		   
+	    callback();
+	   
+});
+vorpal
+  .command('account show <extid>')
   .option('--privatekey','Print Private Key.')
-  .action(function (args, callback) {
+  .action(function (args, callback) {	 
     var node = new StromDAONode.Node({external_id:args.extid,testMode:true,rpc:rpcurl,abilocation:"https://cdn.rawgit.com/energychain/StromDAO-BusinessObject/master/smart_contracts/"});
-	
+	this.log(vorpal._delimiter);
     this.log("Address",node.wallet.address);
     if (args.options.privatekey) {
 		this.log("PrivateKey",node.wallet.privateKey);
@@ -92,7 +155,8 @@ vorpal
 });	
 
 vorpal
-  .command('account import <extid> <privatekey>')  
+  .command('account import <extid> <privatekey>') 
+  .description('Sets a new private key to a external id') 
   .action(function (args, callback) {
     var node = new StromDAONode.Node({external_id:args.extid,privatKey:args.privatekey,testMode:true,rpc:rpcurl,abilocation:"https://cdn.rawgit.com/energychain/StromDAO-BusinessObject/master/smart_contracts/"});
 	
